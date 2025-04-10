17d0075608ae5e8de9ebbfd97fbb7f85a49a2ab7
lijiantong <1243095208@qq.com>
2025-04-10 11:11:59 +0800

    feat: 测试h5分支

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Vue3 + TypeScript + Less + ElementPlust + Pinia 管理后台</h1>
+    <h1>Vue3 + TypeScript + Less + ElementPlust + Pinia 管理后台！</h1>
 </template>
 
 <script lang="ts" setup></script>
