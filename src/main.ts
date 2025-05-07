90074df47840d278cf3fa0080d49b58c0fd86470
lijiantong <1243095208@qq.com>
2025-05-07 16:11:38 +0800

    fix: 配置自动化部署

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ const app = createApp(App);
 
 // 打印环境变量
 console.log(import.meta.env.VITE_APP_NAME, '111');
+
+// 只在生产环境加载 mock 数据
+if (import.meta.env.MODE === 'production') {
+    import('../mockProdServer').then(({ setupProdMockServer }) => {
+        setupProdMockServer();
+    });
+}
 app.use(router);
 app.use(pinia);
 app.mount('#app');
