fc7b3af75814a2ebfe6e953d5f4888404196c740
lijiantong <1243095208@qq.com>
2025-05-07 16:04:37 +0800

    fix: 配置自动化部署

--- mockProdServer.ts
+++ mockProdServer.ts
@@ -1,6 +1,5 @@
-import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer';
+import { createProdMockServer } from 'vite-plugin-mock/client';
 
-// 自动引入 mock 文件夹下的所有 .ts 文件
 export function setupProdMockServer() {
     createProdMockServer(import.meta.glob('./mock/**/*.ts', { eager: true }));
 }
