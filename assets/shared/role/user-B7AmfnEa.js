5682b6d7499a924105046cce07ad50de33cd83ba
LeeJtong <LeeJtong@users.noreply.github.com>
2025-05-07 08:12:54 +0000

    deploy: 90074df47840d278cf3fa0080d49b58c0fd86470

---
+++ assets/shared/role/user-B7AmfnEa.js
@@ -0,0 +1,1 @@
+import{s as t}from"../../common-B5RJ0R9V.js";function r(e){return t({url:"/getRoleList",method:"get",data:e})}export{r as g};
--- assets/shared/role/user-BWzcWQ50.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"../../common-CxcvxXcI.js";function r(e){return t({url:"/getRoleList",method:"get",data:e})}export{r as g};
